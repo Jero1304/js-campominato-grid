ee8bf076c7017f46f591fd2637d5c62094509254
Jero1304 <calogeroleto13@gmail.com>
2023-02-09 17:08:25 +0100

    FEAT stampa numero della cella

--- js/app.js
+++ js/app.js
@@ -1,24 +1,37 @@
 console.log('griglia');
 
-let latoGriglia = 4;
+let latoGriglia = 8;
 let numeroCelle = latoGriglia **2;
 console.log('lato griglia:',latoGriglia,'numero celle:',numeroCelle);
 
 const grigliaEle = document.querySelector('.griglia');
-const gridBtnEle = document.getElementById('grid-btn')
-console.log(grigliaEle,gridBtnEle);
+const gridBtnEle = document.getElementById('grid-btn');
+console.log('griglia:',grigliaEle,'button:',gridBtnEle);
 
 gridBtnEle.addEventListener('click', function(){
     for (let i = 0; i < numeroCelle; i++) {
         let num = i + 1;
         console.log(num);
-
+        
         let divString = ` <div class="cella" style="width: calc(100% / ${latoGriglia});" >${num} </div> `;
-        console.log(divString);
-
+        console.log('stringa:',divString);
         grigliaEle.innerHTML += divString;
+        
+        
+        
+    }
+    
+    const cellaEle = document.querySelectorAll('.cella');
+    for (let i = 0; i < cellaEle.length; i++) {
+        let cellaSingola = cellaEle[i];
+        cellaSingola.addEventListener('click', function(){
+            console.log(`cella numero ${i + 1}`);
+            
+        })
+        
     }
     
 })
+
 
 
