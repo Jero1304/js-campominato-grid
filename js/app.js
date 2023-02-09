97309be3043d4ea9e5e9879ac002adc020f2cd30
Jero1304 <calogeroleto13@gmail.com>
2023-02-09 19:13:54 +0100

    FEAT reset grid

--- js/app.js
+++ js/app.js
@@ -1,21 +1,21 @@
 console.log('griglia');
 
-let latoGriglia = 2;
+let latoGriglia = 4;
 let numeroCelle = latoGriglia **2;
 console.log('lato griglia:',latoGriglia,'numero celle:',numeroCelle);
 
 const grigliaEle = document.querySelector('.griglia');
 const gridBtnEle = document.getElementById('grid-btn');
+
 // console.log('griglia:',grigliaEle,'button:',gridBtnEle);
 
 gridBtnEle.addEventListener('click', function(){
-
+    
+    grigliaEle.innerHTML = '';
     for (let i = 0; i < numeroCelle; i++) {
         let num = i + 1;
-        // console.log(num);
         
         let divString = ` <div class="cella" style="width: calc(100% / ${latoGriglia});" >${num} </div> `;
-        // console.log('stringa:',divString);
         grigliaEle.innerHTML += divString;
     }
     
@@ -28,10 +28,9 @@ gridBtnEle.addEventListener('click', function(){
             cellaSingola.classList.add('bg-primary');
         })
     }
-
-    
     
 })
+
 
 
 
